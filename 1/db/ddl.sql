5ea46266494ed5bc251a2b20091c04fe13da8b3c
f01c33 <felchar@proton.me>
2024-03-30 13:08:48 -0300

    ddl

---
+++ 1/db/ddl.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE tb01
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE TABLE IF NOT EXISTS tb01 (
+    id integer PRIMARY KEY,
+    col_texto text,
+    col_dt timestamp
+);
